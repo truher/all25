5d6896d95fc6da982044e34ef513c6c5b0442b6d
Joel Truher <joel@truher.org>
2025-01-30 22:22:01 -0800

    update memos in timeless tests

--- lib/src/test/java/org/team100/lib/testing/Timeless.java
+++ lib/src/test/java/org/team100/lib/testing/Timeless.java
@@ -2,6 +2,7 @@ package org.team100.lib.testing;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.team100.lib.util.Memo;
 import org.team100.lib.util.Takt;
 
 import edu.wpi.first.hal.HAL;
@@ -29,6 +30,8 @@ public interface Timeless {
     default void stepTime(double t) {
         SimHooks.stepTiming(t);
         Takt.update();
+        Memo.resetAll();
+        Memo.updateAll();
     }
 
 }
