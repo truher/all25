ec091d8a288fd6323434de21328866ab9d3af8d9
Joel Truher <joel@truher.org>
2025-02-05 21:26:53 -0800

    starting to remove old followers

---
+++ lib/src/main/java/org/team100/lib/follower/FieldRelativeDriveTrajectoryFollower.java
@@ -0,0 +1,27 @@
+package org.team100.lib.follower;
+
+import org.team100.lib.dashboard.Glassy;
+import org.team100.lib.motion.drivetrain.SwerveModel;
+import org.team100.lib.motion.drivetrain.kinodynamics.FieldRelativeVelocity;
+import org.team100.lib.trajectory.TrajectoryTimeIterator;
+
+/**
+ * This is a version of the DriveTrajectoryFollower idea, but using
+ * field-relative control instead of robot-relative control.
+ */
+public interface FieldRelativeDriveTrajectoryFollower extends Glassy {
+
+    void setTrajectory(TrajectoryTimeIterator trajectory);
+
+    /**
+     * Makes no attempt to enforce feasibility.
+     * 
+     * @param timestamp   in seconds, use Takt.get(). TODO: remove this
+     * @param measurement measured state
+     * @return velocity control
+     */
+    FieldRelativeVelocity update(double timestamp, SwerveModel measurement);
+
+    boolean isDone();
+
+}
