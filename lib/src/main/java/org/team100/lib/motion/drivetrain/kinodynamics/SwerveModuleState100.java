a53049b30cfb696af668732f70191dc1e5782e75
truher <joel@truher.org>
2025-02-08 16:42:28 -0800

    cleaning

--- lib/src/main/java/org/team100/lib/motion/drivetrain/kinodynamics/SwerveModuleState100.java
+++ lib/src/main/java/org/team100/lib/motion/drivetrain/kinodynamics/SwerveModuleState100.java
@@ -7,85 +7,57 @@ import org.team100.lib.geometry.GeometryUtil;
 import org.team100.lib.swerve.SwerveUtil;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.proto.SwerveModuleStateProto;
 import edu.wpi.first.math.kinematics.struct.SwerveModuleStateStruct;
-import edu.wpi.first.util.protobuf.ProtobufSerializable;
 import edu.wpi.first.util.struct.StructSerializable;
 
-/**
- * This is a copy of {@link edu.wpi.first.math.kinematics.SwerveModuleState} but
- * with second-order fields.
- * 
- * Represents the state of one swerve module.
- * 
- * TODO: this might be safer/simpler as an x and y velocity instead of a speed
- * and an angle.
- */
-public class SwerveModuleState100
-        implements Comparable<SwerveModuleState100>, ProtobufSerializable, StructSerializable {
+/** The state of one swerve module. */
+public class SwerveModuleState100 implements Comparable<SwerveModuleState100>, StructSerializable {
+    private final double m_speedM_S;
+    private final Optional<Rotation2d> m_angle;
 
-    private double speedMetersPerSecond;
-    private Optional<Rotation2d> angle = Optional.empty();
-
-    /** SwerveModuleState protobuf for serialization. */
-    public static final SwerveModuleStateProto proto = new SwerveModuleStateProto();
-
-    /** SwerveModuleState struct for serialization. */
-    public static final SwerveModuleStateStruct struct = new SwerveModuleStateStruct();
-
-    /**
-     * Constructs a SwerveModuleState with zeros for speed and *indeterminate*
-     * angle.
-     */
+    /** Zero speed and indeterminate angle. */
     public SwerveModuleState100() {
+        this(0, Optional.empty());
     }
 
-    /**
-     * Constructs a SwerveModuleState.
-     *
-     * @param speedMetersPerSecond The speed of the wheel of the module.
-     * @param angle                The angle of the module.
-     */
-    public SwerveModuleState100(
-            double speedMetersPerSecond,
-            Optional<Rotation2d> angle) {
-        this.speedMetersPerSecond = speedMetersPerSecond;
-        this.angle = angle;
+    public SwerveModuleState100(double speedM_S, Optional<Rotation2d> angle) {
+        m_speedM_S = speedM_S;
+        m_angle = angle;
     }
 
-    /** Replace empty angle in this state with the supplied angle */
+    /** Replace empty angle in this state with the supplied angle. */
     SwerveModuleState100 overwriteEmpty(SwerveModuleState100 other) {
-        if (angle.isPresent())
+        if (m_angle.isPresent())
             return this;
-        return new SwerveModuleState100(speedMetersPerSecond, other.angle);
+        return new SwerveModuleState100(m_speedM_S, other.m_angle);
     }
 
     SwerveModuleState100 scale(double scale) {
-        return new SwerveModuleState100(speedMetersPerSecond * scale, angle);
+        return new SwerveModuleState100(m_speedM_S * scale, m_angle);
     }
 
     /** Overwrite the states with the supplied steering overrides, if any. */
-    public SwerveModuleState100 override(Rotation2d override) {
-        if (angle.isEmpty())
+    SwerveModuleState100 override(Rotation2d override) {
+        if (m_angle.isEmpty())
             return this;
         if (override == null)
             return this;
-        if (SwerveUtil.shouldFlip(override.minus(angle.get()))) {
-            return new SwerveModuleState100(speedMetersPerSecond * -1.0, Optional.of(override));
+        if (SwerveUtil.shouldFlip(override.minus(m_angle.get()))) {
+            return new SwerveModuleState100(m_speedM_S * -1.0, Optional.of(override));
         }
-        return new SwerveModuleState100(speedMetersPerSecond, Optional.of(override));
+        return new SwerveModuleState100(m_speedM_S, Optional.of(override));
     }
 
     /** Flip direction and angle if this is within 90 degrees of the previous. */
-    public SwerveModuleState100 flipIfRequired(SwerveModuleState100 prev) {
-        if (angle.isEmpty() || prev.angle.isEmpty())
+    SwerveModuleState100 flipIfRequired(SwerveModuleState100 prev) {
+        if (m_angle.isEmpty() || prev.m_angle.isEmpty())
             return this;
-        final Rotation2d deltaRotation = angle.get().minus(prev.angle.get());
+        final Rotation2d deltaRotation = m_angle.get().minus(prev.m_angle.get());
         if (!SwerveUtil.shouldFlip(deltaRotation))
             return this;
         return new SwerveModuleState100(
-                speedMetersPerSecond *= -1.0,
-                Optional.of(GeometryUtil.flip(angle.get())));
+                m_speedM_S * -1.0,
+                Optional.of(GeometryUtil.flip(m_angle.get())));
 
     }
 
@@ -101,38 +73,33 @@ public class SwerveModuleState100
      */
     public static SwerveModuleState100 optimize(
             SwerveModuleState100 desiredState, Rotation2d currentAngle) {
-        // this does happen
-        if (desiredState.angle.isEmpty()) {
-            // Util.warn("SwerveModuleState100.optimize: empty angle!");
+        if (desiredState.m_angle.isEmpty()) {
+            // this does happen
             return desiredState;
         }
-        var delta = desiredState.angle.get().minus(currentAngle);
+        var delta = desiredState.m_angle.get().minus(currentAngle);
         if (Math.abs(delta.getDegrees()) > 90.0) {
             return new SwerveModuleState100(
-                    -desiredState.speedMetersPerSecond,
-                    Optional.of(desiredState.angle.get().rotateBy(Rotation2d.fromDegrees(180.0))));
+                    -desiredState.m_speedM_S,
+                    Optional.of(desiredState.m_angle.get().rotateBy(Rotation2d.fromDegrees(180.0))));
         } else {
             return new SwerveModuleState100(
-                    desiredState.speedMetersPerSecond,
-                    desiredState.angle);
+                    desiredState.m_speedM_S,
+                    desiredState.m_angle);
         }
     }
 
-    public double deltaV(SwerveModuleState100 other) {
-        return speedMetersPerSecond - other.speedMetersPerSecond;
-    }
-
-    public double limit(SwerveModuleState100 next, double limit) {
-        if (Math.abs(next.speedMetersPerSecond) <= limit)
+    double limit(SwerveModuleState100 next, double limit) {
+        if (Math.abs(next.m_speedM_S) <= limit)
             return 1.0;
-        double d = Math.abs(next.speedMetersPerSecond) - Math.abs(speedMetersPerSecond);
-        double dd = limit - Math.abs(speedMetersPerSecond);
+        double d = Math.abs(next.m_speedM_S) - Math.abs(m_speedM_S);
+        double dd = limit - Math.abs(m_speedM_S);
         return dd / d;
     }
 
     /** Speed of the wheel of the module. */
     public double speedMetersPerSecond() {
-        return speedMetersPerSecond;
+        return m_speedM_S;
     }
 
     /**
@@ -140,19 +107,19 @@ public class SwerveModuleState100
      * indeterminate (e.g. calculating the angle required for zero speed).
      */
     public Optional<Rotation2d> angle() {
-        return angle;
+        return m_angle;
     }
 
     @Override
     public String toString() {
         return String.format(
                 "SwerveModuleState(Speed: %.2f m/s, Angle: %s)",
-                speedMetersPerSecond, angle);
+                m_speedM_S, m_angle);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(speedMetersPerSecond, angle);
+        return Objects.hash(m_speedM_S, m_angle);
     }
 
     /**
@@ -164,17 +131,19 @@ public class SwerveModuleState100
      */
     @Override
     public int compareTo(SwerveModuleState100 other) {
-        return Double.compare(this.speedMetersPerSecond, other.speedMetersPerSecond);
+        return Double.compare(this.m_speedM_S, other.m_speedM_S);
     }
 
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof SwerveModuleState100) {
             SwerveModuleState100 other = (SwerveModuleState100) obj;
-            return Math.abs(other.speedMetersPerSecond - speedMetersPerSecond) < 1E-9
-                    && angle.equals(other.angle);
+            return Math.abs(other.m_speedM_S - m_speedM_S) < 1E-9
+                    && m_angle.equals(other.m_angle);
         }
         return false;
     }
 
+    /** SwerveModuleState struct for serialization. */
+    public static final SwerveModuleStateStruct struct = new SwerveModuleStateStruct();
 }
