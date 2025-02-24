54b7ce1ca0784123188667c11261a9217dcaef10
Joel Truher <joel@truher.org>
2025-02-24 14:06:04 -0800

    add cross-track-error reduction

--- lib/src/test/java/org/team100/lib/commands/drivetrain/RotateTest.java
+++ lib/src/test/java/org/team100/lib/commands/drivetrain/RotateTest.java
@@ -15,10 +15,26 @@ import org.team100.lib.motion.drivetrain.Fixtured;
 import org.team100.lib.motion.drivetrain.kinodynamics.SwerveKinodynamics;
 import org.team100.lib.motion.drivetrain.kinodynamics.SwerveKinodynamicsFactory;
 import org.team100.lib.testing.Timeless;
+import org.team100.lib.util.Util;
 
 class RotateTest extends Fixtured implements Timeless {
+    private static final boolean DEBUG = false;
     private static final double kDelta = 0.02;
     private static final LoggerFactory logger = new TestLoggerFactory(new TestPrimitiveLogger());
+
+    private void verify(double speed, double angle) {
+        assertEquals(speed, fixture.drive.getSwerveLocal().getDesiredStates().frontLeft().speedMetersPerSecond(),
+                kDelta);
+        assertEquals(angle, fixture.drive.getSwerveLocal().getDesiredStates().frontLeft().angle().get().getRadians(),
+                kDelta);
+    }
+
+    private void print() {
+        if (DEBUG)
+            Util.printf("%6.3f %6.3f\n",
+                    fixture.drive.getSwerveLocal().getDesiredStates().frontLeft().speedMetersPerSecond(),
+                    fixture.drive.getSwerveLocal().getDesiredStates().frontLeft().angle().get().getRadians());
+    }
 
     @Test
     void testRotate() {
@@ -27,7 +43,7 @@ class RotateTest extends Fixtured implements Timeless {
         SwerveKinodynamics swerveKinodynamics = SwerveKinodynamicsFactory.forTest();
         // remember the test rotation rate is *very* slow.
         assertEquals(2.828, swerveKinodynamics.getMaxAngleSpeedRad_S(), 0.001);
-        
+
         double targetAngle = Math.PI / 2;
         SwerveController controller = SwerveControllerFactory.test(logger);
         Rotate rotate = new Rotate(
@@ -38,56 +54,43 @@ class RotateTest extends Fixtured implements Timeless {
 
         rotate.initialize();
 
-        assertEquals(0, fixture.drive.getSwerveLocal().getDesiredStates().frontLeft().speedMetersPerSecond(), kDelta);
-        assertEquals(0, fixture.drive.getSwerveLocal().getDesiredStates().frontLeft().angle().get().getRadians(), kDelta);
+        verify(0, 0);
+        print();
 
-        // steering
+        // since "steer at rest" is off, this turns immediately.
         for (int i = 0; i < 13; ++i) {
             stepTime();
             fixture.drive.periodic();
             rotate.execute();
+            print();
         }
-        // now we're ready to start rotating
-        assertEquals(-0.417, fixture.drive.getSwerveLocal().getDesiredStates().frontLeft().speedMetersPerSecond(),
-                kDelta);
-        assertEquals(-Math.PI / 4,
-                fixture.drive.getSwerveLocal().getDesiredStates().frontLeft().angle().get().getRadians(), 0.1);
+        // going pretty fast
+        verify(-0.417, -Math.PI / 4);
 
-        // finished steering, start rotating
         for (int i = 0; i < 25; ++i) {
             stepTime();
             fixture.drive.periodic();
             rotate.execute();
+            print();
         }
-        assertEquals(-0.512, fixture.drive.getSwerveLocal().getDesiredStates().frontLeft().speedMetersPerSecond(),
-                kDelta);
-        assertEquals(-Math.PI / 4,
-                fixture.drive.getSwerveLocal().getDesiredStates().frontLeft().angle().get().getRadians(), 0.1);
+        // still going
+        verify(-0.512, -Math.PI / 4);
 
-        // should be done rotating now
-        for (int i = 0; i < 50; ++i) {
+        for (int i = 0; i < 35; ++i) {
             stepTime();
             fixture.drive.periodic();
             rotate.execute();
+            print();
         }
 
+        // done
+        verify(0, -Math.PI / 4);
 
-        assertEquals(-Math.PI / 4,
-                fixture.drive.getSwerveLocal().getDesiredStates().frontLeft().angle().get().getRadians(),
-                kDelta);
-
-        for (int i = 0; i < 113; ++i) {
-            stepTime();
-            fixture.drive.periodic();
-            rotate.execute();
-        }
-
+        // note we say we're done slightly before we actually are due to the tolerance,
+        // which is fine.
         assertTrue(rotate.isFinished());
 
         rotate.end(false);
-        assertEquals(0, fixture.drive.getSwerveLocal().getDesiredStates().frontLeft().speedMetersPerSecond(), kDelta);
-        assertEquals(-Math.PI / 4,
-                fixture.drive.getSwerveLocal().getDesiredStates().frontLeft().angle().get().getRadians(),
-                kDelta);
+        verify(0, -Math.PI / 4);
     }
 }
