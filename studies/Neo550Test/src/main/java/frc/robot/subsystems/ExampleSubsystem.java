6feb08a680dc8aa2b4d67311597291391b24d3ef
GeniusKoi <kymontauk@gmail.com>
2025-02-25 17:49:57 -0800

    made but not checked or tested current limiter

---
+++ studies/Neo550Test/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -0,0 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ExampleSubsystem extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+
+  SparkMax number1Max ;
+  public ExampleSubsystem() {
+    number1Max = new SparkMax(27, MotorType.kBrushless);
+    SparkMaxConfig conf = new SparkMaxConfig();
+    conf.smartCurrentLimit(20, 20);
+    number1Max.configure(conf, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+
+  }
+
+  public void setMotor(){
+    number1Max.set(0.5);
+  
+    
+  }
+
+  public void setNone(){
+    number1Max.set(0);
+  }
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
