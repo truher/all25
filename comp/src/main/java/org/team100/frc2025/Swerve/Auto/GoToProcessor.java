b2bd63250569479d62ececfa0f8daef0f82687b5
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2025-03-15 13:15:47 -0700

    Added to go processor

---
+++ comp/src/main/java/org/team100/frc2025/Swerve/Auto/GoToProcessor.java
@@ -0,0 +1,25 @@
+package org.team100.frc2025.Swerve.Auto;
+
+import org.team100.frc2025.Wrist.AlgaeGrip;
+import org.team100.frc2025.Wrist.OuttakeAlgaeGrip;
+import org.team100.lib.commands.drivetrain.DriveToPoseWithProfile;
+import org.team100.lib.controller.drivetrain.SwerveController;
+import org.team100.lib.logging.FieldLogger;
+import org.team100.lib.motion.drivetrain.SwerveDriveSubsystem;
+import org.team100.lib.motion.drivetrain.SwerveModel;
+import org.team100.lib.profile.HolonomicProfile;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class GoToProcessor extends SequentialCommandGroup {
+    public GoToProcessor(FieldLogger.Log fieldLog, SwerveDriveSubsystem m_drive, SwerveController holonomicController,
+            HolonomicProfile profile, AlgaeGrip algaeGrip) {
+        addCommands(
+                new DriveToPoseWithProfile(fieldLog,
+                        () -> new SwerveModel(new Pose2d(6.187, 0.626, new Rotation2d(3*Math.PI / 2))),
+                        m_drive, holonomicController, profile),
+                new OuttakeAlgaeGrip(algaeGrip));
+    }
+}
