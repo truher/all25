42f72a8725e99841eee75f11285d1a4548e7f99e
Joel Truher <joel@truher.org>
2025-04-09 15:46:15 -0700

    fiddling with profiles

--- lib/src/main/java/org/team100/lib/profile/timed/CompleteProfile.java
+++ lib/src/main/java/org/team100/lib/profile/timed/CompleteProfile.java
@@ -3,8 +3,11 @@ package org.team100.lib.profile.timed;
 import org.team100.lib.profile.IncrementalProfile;
 import org.team100.lib.state.Control100;
 import org.team100.lib.state.Model100;
+import org.team100.lib.util.Util;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
+import edu.wpi.first.math.interpolation.Interpolator;
 import edu.wpi.first.math.interpolation.InverseInterpolator;
 
 /**
@@ -37,27 +40,122 @@ import edu.wpi.first.math.interpolation.InverseInterpolator;
  * be precalculated (and because 100% of our actual paths have stationary
  * goals).
  * 
- * It's similar to a sliding-mode controller: maximum effort to reach a sliding
+ * It's similar to a sliding-mode controller: maximum effort to reach a
  * manifold, and then moving alnog it.
  */
 public class CompleteProfile implements IncrementalProfile {
-    InterpolatingTreeMap<Double, Control100> m_byDistance;
+    private static final boolean DEBUG = true;
+    private static final double DT = 0.02;
 
-    public CompleteProfile() {
-        // InverseInterpolator<Double> keyInterpolator = InverseInterpolator.forDouble();
-        // Interpolator<Control100> valueInterpolator = 
-        // m_byDistance = new InterpolatingTreeMap<>();
+    private final double m_maxV;
+    private final double m_maxA;
+    private final double m_tolerance;
+    final InterpolatingTreeMap<Double, Control100> m_byDistance;
+
+    public CompleteProfile(double maxV, double maxA, double tolerance) {
+        m_maxV = maxV;
+        m_maxA = maxA;
+        m_tolerance = tolerance;
+        InverseInterpolator<Double> keyInterpolator = InverseInterpolator.forDouble();
+        Interpolator<Control100> valueInterpolator = Control100::interpolate;
+        m_byDistance = new InterpolatingTreeMap<>(keyInterpolator, valueInterpolator);
+        // this is the goal state, zero control here.
+        Control100 c = new Control100();
+        m_byDistance.put(0.0, c);
+        // control from the left, walking back in time
+        double a = -maxA;
+        for (int i = 0; i < 100; ++i) {
+            c = new Control100(
+                    c.x() - c.v() * DT - 0.5 * a * DT * DT,
+                    c.v() - a * DT,
+                    a);
+            m_byDistance.put(c.x(), c);
+        }
+        // control from the right, walking back in time
+        a = maxA;
+        c = new Control100();
+        for (int i = 0; i < 100; ++i) {
+            c = new Control100(
+                    c.x() - c.v() * DT - 0.5 * a * DT * DT,
+                    c.v() - a * DT,
+                    a);
+            m_byDistance.put(c.x(), c);
+        }
     }
 
     @Override
     public Control100 calculate(double dt, Model100 setpoint, Model100 goal) {
         if (Math.abs(goal.v()) > 1e-6)
             throw new IllegalArgumentException("This profile works only with stationary goals.");
-        double togo = goal.x() - setpoint.x();
-        Control100 lerp = m_byDistance.get(togo);
-        // if (setpoint.v())
+        if (DEBUG)
+            Util.printf("setpoint %s goal %s\n", setpoint, goal);
 
-            return new Control100();
+        // negative number = setpoint is to the left.
+        double togo = setpoint.x() - goal.x();
+        if (MathUtil.isNear(0, togo, m_tolerance)) {
+            if (DEBUG)
+                Util.printf("at goal, togo %s\n", togo);
+            return goal.control();
+        }
+        Control100 lerp = m_byDistance.get(togo);
+        if (DEBUG)
+            Util.printf("togo %f lerp %s\n", togo, lerp);
+        if (togo < 0) {
+            if (DEBUG)
+                Util.println("goal is to the right");
+            if (setpoint.v() + m_tolerance < lerp.v()) {
+                // setpoint is below the goal path, go faster to get there.
+                if (DEBUG)
+                    Util.println("setpoint velocity is less positive than goal path: speed up");
+                return new Control100(
+                        setpoint.x() + setpoint.v() * dt + 0.5 * m_maxA * dt * dt,
+                        setpoint.v() + m_maxA * dt,
+                        m_maxA);
+            } else if (setpoint.v() - m_tolerance < lerp.v()) {
+                if (DEBUG)
+                    Util.println("setpoint is within tolerance of lerp");
+                return new Control100(
+                        goal.x() + lerp.x() + lerp.v() * dt + 0.5 * lerp.a() * dt * dt,
+                        lerp.v() + lerp.a() * dt,
+                        lerp.a());
+            } else {
+                // setpoint is above the goal path, slow down to get there.
+                if (DEBUG)
+                    Util.println("setpoint velocity is more positive than goal path: slow down");
+                return new Control100(
+                        setpoint.x() + setpoint.v() * dt - 0.5 * m_maxA * dt * dt,
+                        setpoint.v() - m_maxA * dt,
+                        -m_maxA);
+            }
+        } else {
+            if (DEBUG)
+                Util.println("goal is to the left: use negative velocity");
+            if (setpoint.v() - m_tolerance > lerp.v()) {
+                // setpoint is above the goal path, go faster (to the left) to get there.
+                if (DEBUG)
+                    Util.println("setpoint velocity is less negative than goal path: speed up");
+                return new Control100(
+                        setpoint.x() + setpoint.v() * dt - 0.5 * m_maxA * dt * dt,
+                        setpoint.v() - m_maxA * dt,
+                        -m_maxA);
+            } else if (setpoint.v() + m_tolerance > lerp.v()) {
+                if (DEBUG)
+                    Util.println("setpoint is within tolerance of lerp");
+                return new Control100(
+                        goal.x() + lerp.x() + lerp.v() * dt + 0.5 * lerp.a() * dt * dt,
+                        lerp.v() + lerp.a() * dt,
+                        lerp.a());
+            } else {
+                // setpoint is below the goal path, slow down (i.e. push to the right) to get
+                // there.
+                if (DEBUG)
+                    Util.println("setpoint velocity is more negative than goal path: slow down");
+                return new Control100(
+                        setpoint.x() + setpoint.v() * dt + 0.5 * m_maxA * dt * dt,
+                        setpoint.v() + m_maxA * dt,
+                        m_maxA);
+            }
+        }
     }
 
 }
--- lib/src/test/java/org/team100/lib/profile/timed/CompleteProfileTest.java
+++ lib/src/test/java/org/team100/lib/profile/timed/CompleteProfileTest.java
@@ -1,5 +1,7 @@
 package org.team100.lib.profile.timed;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 import org.team100.lib.state.Control100;
 import org.team100.lib.state.Model100;
@@ -7,21 +9,44 @@ import org.team100.lib.util.Util;
 
 public class CompleteProfileTest {
     private static final boolean DEBUG = true;
+    private static final double DT = 0.02;
+
+    /** Dump the sliding mode surface */
+    @Test
+    void testMode() {
+        CompleteProfile p = new CompleteProfile(2, 6, 0.01);
+        if (DEBUG) {
+            for (double x = -10; x < 10; x += 0.01) {
+                Control100 sample = p.m_byDistance.get(x);
+                Util.printf("%12.3f %12.3f %12.3f\n", sample.x(), sample.v(), sample.a());
+            }
+        }
+    }
 
     @Test
     void testSimple() {
-        // use a higher resolution to make a nice looking graph.
-        // CompleteProfile p = new CompleteProfile(2, 6, 20, 0.01);
-        // p.init(new Control100(), new Model100(1, 0));
-        // for (double t = 0; t < 2; t += 0.01) {
-        //     Control100 c = p.sample(t);
-        //     if (DEBUG)
-        //         Util.printf("%12.3f %12.3f %12.3f %12.3f\n", t, c.x(), c.v(), c.a());
-        // }
+        CompleteProfile p = new CompleteProfile(2, 6, 0.01);
+        final Model100 goal = new Model100(1, 0);
+        Control100 c = new Control100();
+        double t = 0;
+        for (int i = 0; i < 300; ++i) {
+            if (DEBUG)
+                Util.printf("%12.3f %12.3f %12.3f %12.3f\n", t, c.x(), c.v(), c.a());
+            c = p.calculate(DT, c.model(), goal);
+            t += DT;
+        }
+    }
+
+    /** Moving goals are not allowed. */
+    @Test
+    void testMovingGoal() {
+        CompleteProfile p = new CompleteProfile(2, 6, 0.01);
+        assertThrows(IllegalArgumentException.class,
+                () -> p.calculate(0.02, new Model100(), new Model100(1, 1)));
     }
 
     @Test
     void testKeyInterpolation() {
-        
+
     }
 }
