cba01e4e67b2f074d8ce23f47f58570ef6687e0e
Joel Truher <joel@truher.org>
2025-04-09 11:27:37 -0700

    new profile

--- lib/src/main/java/org/team100/lib/profile/timed/CompleteProfile.java
+++ lib/src/main/java/org/team100/lib/profile/timed/CompleteProfile.java
@@ -4,7 +4,8 @@ import org.team100.lib.profile.IncrementalProfile;
 import org.team100.lib.state.Control100;
 import org.team100.lib.state.Model100;
 
-import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
+import edu.wpi.first.math.interpolation.InverseInterpolator;
 
 /**
  * A simple profile to implement all the things we want from a
@@ -31,13 +32,32 @@ import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
  * the goal path, then states are interpolated from it.
  * 
  * so this is actually an incremental profile.
+ * 
+ * Note that only stationary goal states are allowed, so that the goal path can
+ * be precalculated (and because 100% of our actual paths have stationary
+ * goals).
+ * 
+ * It's similar to a sliding-mode controller: maximum effort to reach a sliding
+ * manifold, and then moving alnog it.
  */
 public class CompleteProfile implements IncrementalProfile {
+    InterpolatingTreeMap<Double, Control100> m_byDistance;
 
-  
+    public CompleteProfile() {
+        // InverseInterpolator<Double> keyInterpolator = InverseInterpolator.forDouble();
+        // Interpolator<Control100> valueInterpolator = 
+        // m_byDistance = new InterpolatingTreeMap<>();
+    }
+
     @Override
     public Control100 calculate(double dt, Model100 setpoint, Model100 goal) {
-        return new Control100();
+        if (Math.abs(goal.v()) > 1e-6)
+            throw new IllegalArgumentException("This profile works only with stationary goals.");
+        double togo = goal.x() - setpoint.x();
+        Control100 lerp = m_byDistance.get(togo);
+        // if (setpoint.v())
+
+            return new Control100();
     }
 
 }
