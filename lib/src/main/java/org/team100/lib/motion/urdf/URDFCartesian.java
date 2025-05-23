836e8b9e4dd1058db27510cf1fff33072c723d50
truher <joel@truher.org>
2025-05-23 16:36:32 -0700

    starting on cartesian

--- lib/src/main/java/org/team100/lib/motion/urdf/URDFCartesian.java
+++ lib/src/main/java/org/team100/lib/motion/urdf/URDFCartesian.java
@@ -2,17 +2,47 @@ package org.team100.lib.motion.urdf;
 
 import java.util.List;
 
+import org.team100.lib.motion.urdf.URDFModel.Joint;
+import org.team100.lib.motion.urdf.URDFModel.Joint.JointType;
+import org.team100.lib.motion.urdf.URDFModel.Joint.Limit;
+import org.team100.lib.motion.urdf.URDFModel.Link;
 import org.team100.lib.motion.urdf.URDFModel.Robot;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose3d;
+
 /**
- * Cartesian prismatic, like the Haas mill.
+ * Cartesian prismatic, like the Shopsabre:
+ * * base is the table at the origin
+ * * gantry moves in y
+ * * headstock moves in x (along the gantry)
+ * * spindle moves in z (along the headstock)
  */
 public class URDFCartesian {
     public static final Robot ROBOT;
     static {
-            ROBOT = new Robot(
+        Link base = new Link("base");
+        Link gantry = new Link("gantry");
+        Link head_stock = new Link("head_stock");
+        Link spindle = new Link("spindle");
+
+        ROBOT = new Robot(
                 "Cartesian",
-                List.of(), 
-                List.of());
-        }
+                List.of(
+                        base,
+                        gantry,
+                        head_stock,
+                        spindle),
+                List.of(
+                    new Joint(
+                        "base_gantry",
+                        JointType.prismatic,
+                        new Limit(1000, 0, 1, 1),
+                        base,
+                        gantry,
+                        new Pose3d(),
+                        VecBuilder.fill(0, 1, 0)
+                    );
+                ));
+    }
 }
