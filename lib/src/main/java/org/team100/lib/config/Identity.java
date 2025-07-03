049a77b26eda853324481ce5785d42bf46244d85
truher <joel@truher.org>
2025-07-02 17:20:50 -0700

    fix identity for systemcore controller

--- lib/src/main/java/org/team100/lib/config/Identity.java
+++ lib/src/main/java/org/team100/lib/config/Identity.java
@@ -1,5 +1,8 @@
 package org.team100.lib.config;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +10,7 @@ import org.team100.lib.util.Util;
 
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /*
  * Represents a specific RoboRIO, as a key for configurations.
@@ -29,15 +33,17 @@ public enum Identity {
     SQUAREBOT("031e31e3"),
     SWERVE_TWO("0317f285"),
 
-    COMP_BOT("03238232"), 
-    SWERVE_ONE("032363AC"), 
+    COMP_BOT("03238232"),
+    SWERVE_ONE("032363AC"),
+
+    SYSTEMCORE("35af8fca4c12ad8e"), // new controller for 2027
 
     DISABLED("disabled"), // for mechanisms which don't exist
     BLANK(""), // e.g. test default or simulation
     UNKNOWN(null);
     // FRC_100_ea4("03238232"),
     // COMP_BOT("0306cea4");
-    
+
     private static final Map<String, Identity> identities = new HashMap<>();
 
     static {
@@ -63,14 +69,34 @@ public enum Identity {
             // SEGVs because it does the wrong
             // thing with JNIs, so don't do that.
             serialNumber = RobotController.getSerialNumber();
+            if (serialNumber.isBlank()) {
+                // try again
+                try (BufferedReader reader = new BufferedReader(new FileReader("/proc/cpuinfo"))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        if (line.startsWith("Serial")) {
+                            String[] parts = line.split(":");
+                            if (parts.length > 1) {
+                                serialNumber = parts[1].trim();
+                                break;
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+                    //
+                }
+            }
         } else {
             serialNumber = "";
         }
+        SmartDashboard.putString("serialNumber", serialNumber);
         if (identities.containsKey(serialNumber)) {
             Identity identity = identities.get(serialNumber);
             Util.printf("Identity: %s\n", identity);
+            SmartDashboard.putString("identity", identity.name());
             return identity;
         }
+        SmartDashboard.putString("identity", "UNKNOWN");
         Util.println("Identity: UNKNOWN");
         return UNKNOWN;
     }
