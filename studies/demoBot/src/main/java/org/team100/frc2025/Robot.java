34c54bb40488ec8790609332eb15e40db78b338c
Joel Truher <joel@truher.org>
2025-09-08 07:49:29 -0700

    fix demobot

--- studies/demoBot/src/main/java/org/team100/frc2025/Robot.java
+++ studies/demoBot/src/main/java/org/team100/frc2025/Robot.java
@@ -2,6 +2,8 @@ package org.team100.frc2025;
 
 import java.io.IOException;
 
+import org.team100.lib.coherence.Cache;
+import org.team100.lib.coherence.Takt;
 import org.team100.lib.config.Identity;
 import org.team100.lib.experiments.Experiment;
 import org.team100.lib.experiments.Experiments;
@@ -13,9 +15,6 @@ import org.team100.lib.logging.LoggerFactory.BooleanLogger;
 import org.team100.lib.logging.LoggerFactory.DoubleLogger;
 import org.team100.lib.logging.LoggerFactory.IntLogger;
 import org.team100.lib.logging.Logging;
-import org.team100.lib.util.Debug;
-import org.team100.lib.util.Memo;
-import org.team100.lib.util.Takt;
 import org.team100.lib.util.Util;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -26,7 +25,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.WPILibVersion;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot100 implements Debug {
+public class Robot extends TimedRobot100 {
+    private static final boolean DEBUG = false;
     private final DoubleLogger m_log_ds_MatchTime;
     private final BooleanLogger m_log_ds_AutonomousEnabled;
     private final BooleanLogger m_log_ds_TeleopEnabled;
@@ -142,22 +142,9 @@ public class Robot extends TimedRobot100 implements Debug {
 
     /** Update the measurement caches. */
     private void updateCaches() {
-        // Measure how long the update takes prior to the scheduler running.
         double startUpdateS = Takt.actual();
-
-        // Cache instances hold measurements that we want to keep consistent
-        // for an entire cycle, but that we want to forget between cycles, so we
-        // reset them all here.
-        Memo.resetAll();
-
-        // After the clock is advanced, it would be good to make as many observations as
-        // possible, so the times of those observations are as close to the interrupt
-        // time as possible. This might yield a whole lot of work, though.
-        Memo.updateAll();
-        double endUpdateS = Takt.actual();
-        double updateS = endUpdateS - startUpdateS;
-        // this is how long it takes to update all the memos
-        m_log_update.log(() -> updateS);
+        Cache.refresh();
+        m_log_update.log(() -> (Takt.actual() - startUpdateS));
     }
 
     //////////////////////////////////////////////////////////////////////
@@ -191,7 +178,8 @@ public class Robot extends TimedRobot100 implements Debug {
     public void testInit() {
         clearCommands();
         for (Topic t : NetworkTableInstance.getDefault().getTopics()) {
-           debug("%s\n", t.getName());
+            if (DEBUG)
+                Util.printf("%s\n", t.getName());
         }
     }
 
@@ -217,20 +205,6 @@ public class Robot extends TimedRobot100 implements Debug {
 
     @Override
     public void testPeriodic() {
-        double elevatorControl = NetworkTableInstance.getDefault()
-                .getEntry("log/Elevator/Elevator/Starboard/OutboardLinearPositionServo/control (m)/x").getDouble(0);
-        double wristControl = NetworkTableInstance.getDefault()
-                .getEntry("log/Elevator/Wrist2/OnboardAngularPositionServo/control (rad)/x").getDouble(0);
-        double elevatorMeasurement = NetworkTableInstance.getDefault()
-                .getEntry("log/Elevator/Elevator/Starboard/OutboardLinearPositionServo/position (m)").getDouble(0);
-        double wristMeasurement = NetworkTableInstance.getDefault()
-                .getEntry("log/Elevator/Wrist2/OnboardAngularPositionServo/measurement (rad)/x").getDouble(0);
-        debug("%12.6f %12.6f %12.6f %12.6f\n",
-                elevatorControl / 20,
-                wristControl,
-                elevatorMeasurement / 20,
-                wristMeasurement);
-
     }
 
     //////////////////////////////////////////////////////////////////////
@@ -279,10 +253,5 @@ public class Robot extends TimedRobot100 implements Debug {
         b.append("\n");
         Util.println(b.toString());
 
-    }
-
-    @Override
-    public boolean debug() {
-        return false;
     }
 }
