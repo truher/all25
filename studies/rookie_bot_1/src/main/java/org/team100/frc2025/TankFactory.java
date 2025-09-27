0d14aedbd37b363d6f0917e0d3816b1d89385b33
LukeWepp <luke@weppner.family>
2025-09-27 19:56:05 +0200

    Changed the tank to 2 motors with Neos

--- studies/rookie_bot_1/src/main/java/org/team100/frc2025/TankFactory.java
+++ studies/rookie_bot_1/src/main/java/org/team100/frc2025/TankFactory.java
@@ -1,24 +1,28 @@
 package org.team100.frc2025;
 
+import org.team100.lib.config.Feedforward100;
+import org.team100.lib.config.PIDConstants;
 import org.team100.lib.examples.tank.BareMotorTank;
 import org.team100.lib.examples.tank.TankDrive;
 import org.team100.lib.logging.LoggerFactory;
 import org.team100.lib.motor.BareMotorGroup;
 import org.team100.lib.motor.MotorPhase;
+import org.team100.lib.motor.NeoCANSparkMotor;
 import org.team100.lib.motor.TalonSRXMotor;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 /** Configuration of motors on the Rookie Bot. */
 public class TankFactory {
 
     public static TankDrive make(LoggerFactory parent, int supplyLimit) {
         LoggerFactory log = parent.name("Tank Drive");
-        BareMotorGroup left = new BareMotorGroup(
-                new TalonSRXMotor(log.name("left 1"), 5, MotorPhase.REVERSE, supplyLimit),
-                new TalonSRXMotor(log.name("left 2"), 11, MotorPhase.REVERSE, supplyLimit));
-        BareMotorGroup right = new BareMotorGroup(
-                new TalonSRXMotor(log.name("right 1"), 10, MotorPhase.FORWARD, supplyLimit),
-                new TalonSRXMotor(log.name("right 2"), 3, MotorPhase.FORWARD, supplyLimit));
+
+        NeoCANSparkMotor right = new NeoCANSparkMotor(log, 27, MotorPhase.REVERSE, supplyLimit, Feedforward100.makeNeo(), new PIDConstants());
+
+        NeoCANSparkMotor left = new NeoCANSparkMotor(log, 3, MotorPhase.REVERSE, supplyLimit, Feedforward100.makeNeo(), new PIDConstants());
+       
         return new BareMotorTank(left, right);
     }
-
 }
