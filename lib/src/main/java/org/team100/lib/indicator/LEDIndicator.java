5a6afa948dadcfefa93cb9ac5c7c1717a8a530ff
marcelosin1010 <marcelo.sin1010@gmail.com>
2025-10-02 17:42:46 -0700

    Update LEDIndicator.java

--- lib/src/main/java/org/team100/lib/indicator/LEDIndicator.java
+++ lib/src/main/java/org/team100/lib/indicator/LEDIndicator.java
@@ -30,7 +30,7 @@ import org.team100.lib.coherence.Takt;
  * from green to red as you drive around.
  */
 public class LEDIndicator {
-    private static final int LENGTH = 10;
+    private static final int LENGTH = 40;
     private final AddressableLED m_led;
     // buffer flipping is a little quicker than setting the pixels one at a time
     private final AddressableLEDBuffer m_greenBuffer;
