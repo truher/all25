922c494500ee5ade91f2c3c5478957581fe9b2bc
LincolnMcCarthy <825153@seq.org>
2025-10-15 17:45:30 -0700

    Made auton

--- studies/rookie_bot_2/src/main/java/org/team100/frc2025/TankFactory.java
+++ studies/rookie_bot_2/src/main/java/org/team100/frc2025/TankFactory.java
@@ -1,7 +1,6 @@
 package org.team100.frc2025;
 
 import org.team100.lib.config.Feedforward100;
-import org.team100.lib.config.Identity;
 import org.team100.lib.config.PIDConstants;
 import org.team100.lib.examples.tank.BareMotorTank;
 import org.team100.lib.examples.tank.TankDrive;
@@ -16,16 +15,7 @@ public class TankFactory {
 
     public static TankDrive make(LoggerFactory parent, int supplyLimit) {
         LoggerFactory log = parent.name("Tank Drive");
-        System.out.println("Identity" + Identity.instance);
-        int rightCanId = switch(Identity.instance) {
-            case ROOKIE_BOT -> 1; 
-            default -> 1;
-        };
-        int leftCanId = switch(Identity.instance) {
-            case ROOKIE_BOT ->  1;
-            default -> 1;
-        };
-
+       
         NeoCANSparkMotor right = new NeoCANSparkMotor(log, new CanId(27), NeutralMode.BRAKE,
          MotorPhase.FORWARD, supplyLimit, Feedforward100.makeNeo(), new PIDConstants());
 
