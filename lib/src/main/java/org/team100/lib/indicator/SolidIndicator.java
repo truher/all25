77a46e7f1555082934366bede854854eb78bab6a
Joel Truher <joel@truher.org>
2025-10-21 19:46:22 -0700

    make the indicator a little more opinionated

--- lib/src/main/java/org/team100/lib/indicator/SolidIndicator.java
+++ lib/src/main/java/org/team100/lib/indicator/SolidIndicator.java
@@ -1,89 +1,97 @@
 package org.team100.lib.indicator;
 
+import static edu.wpi.first.wpilibj2.command.Commands.repeatingSequence;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
 
-import org.team100.lib.coherence.Takt;
 import org.team100.lib.util.RoboRioChannel;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
- * An LED indicator that is all the same color, optionally blinking.
+ * An LED indicator that is all the same color.
  * 
- * Blink rate depends on the signal urgency:
+ * There are two kinds of indicator:
  * 
- * * low-urgency signals are steady.
- * * moderately-urgent signals blink at 2.5 Hz at 50% duty cycle.
- * * more-urgent signals blink at 5 Hz at 50% duty cycle.
+ * * steady indicates a continuing state, e.g. "game piece in grip."
+ * 
+ * * blinking indicates an event, e.g. "game piece just acquired."
+ * 
+ * The steady states are supplied to state(), which uses the default command
+ * internally.
+ * 
+ * The events are registered with event(), which uses Triggers.
+ * 
+ * Also serves as an example of using the CommandScheduler.
+ * 
+ * Some guidance for indicator design:
+ * 
+ * * Show things that the drive team otherwise would have a hard time knowing,
+ * e.g. "item acquired," or "localization confidence is high."
+ * 
+ * * Don't duplicate things that the drive team already knows, e.g. "trying to
+ * intake" is just echoing a button.
  * 
  * Resources regarding signal design:
  * 
  * https://www.denix.osd.mil/soh/denix-files/sites/21/2016/03/02_MIL-STD-1472F-Human-Engineering.pdf
  * https://www.faa.gov/regulations_policies/rulemaking/committees/documents/media/TAEas-wcal-04232002.pdf
  */
-public class SolidIndicator {
-    private static final double DUTY_CYCLE = 0.5;
-    private static final double STEADY_HZ = 0;
-    private static final double SLOW_HZ = 1;
-    private static final double FAST_HZ = 4;
-
+public class SolidIndicator extends SubsystemBase {
+    private static final double BLINK_DURATION = 0.06;
+    private static final int EVENT_DURATION = 1;
     private final AddressableLED m_led;
     private final int m_length;
-    private final AddressableLEDBuffer m_black;
     private final Map<Color, AddressableLEDBuffer> m_buffers;
-    private AddressableLEDBuffer m_active;
-    private double m_freq;
 
     public SolidIndicator(RoboRioChannel channel, int length) {
         m_led = new AddressableLED(channel.channel);
         m_length = length;
         m_buffers = new HashMap<>();
-        m_black = fill(Color.kBlack);
-        m_active = m_black;
         m_led.setLength(length);
         m_led.start();
     }
 
-    public void steady(Color color) {
-        setColor(color);
-        setBlinkRate(STEADY_HZ);
+    /**
+     * Show the supplied color.
+     */
+    public void state(Supplier<Color> color) {
+        setDefaultCommand(runOnce(() -> set(color.get())));
     }
 
-    public void slow(Color color) {
-        setColor(color);
-        setBlinkRate(SLOW_HZ);
-    }
-
-    public void fast(Color color) {
-        setColor(color);
-        setBlinkRate(FAST_HZ);
-    }
-
-    public void periodic() {
-        if (m_freq < 0.01 || m_freq > 10) {
-            m_led.setData(m_active);
-            return;
-        }
-        double period = 1 / m_freq;
-        double phase = (Takt.get() % period) / period;
-        if (phase < DUTY_CYCLE) {
-            m_led.setData(m_active);
-        } else {
-            m_led.setData(m_black);
-        }
+    /**
+     * Trigger on true, blink the color a few times.
+     */
+    public void event(BooleanSupplier condition, Color color) {
+        new Trigger(condition).onTrue(blink(color).withTimeout(EVENT_DURATION));
     }
 
     /////////////////////////////////////////////////////////////
 
-    private void setColor(Color color) {
-        m_active = m_buffers.computeIfAbsent(color, this::fill);
+    private Command blink(Color color) {
+        return repeatingSequence(
+                once(color),
+                once(Color.kBlack));
     }
 
-    private void setBlinkRate(double freq) {
-        m_freq = freq;
+    private Command once(Color color) {
+        return run(() -> set(color)).withTimeout(BLINK_DURATION);
+    }
+
+    private void set(Color color) {
+        m_led.setData(forColor(color));
+    }
+
+    private AddressableLEDBuffer forColor(Color color) {
+        return m_buffers.computeIfAbsent(color, this::fill);
     }
 
     private AddressableLEDBuffer fill(Color color) {
--- studies/led/src/main/java/frc/robot/Robot.java
+++ studies/led/src/main/java/frc/robot/Robot.java
@@ -1,7 +1,5 @@
 package frc.robot;
 
-import org.team100.lib.coherence.Cache;
-import org.team100.lib.coherence.Takt;
 import org.team100.lib.indicator.SolidIndicator;
 import org.team100.lib.util.RoboRioChannel;
 
@@ -10,8 +8,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class Robot extends TimedRobot {
 
@@ -22,35 +18,29 @@ public class Robot extends TimedRobot {
         DriverStation.silenceJoystickConnectionWarning(true);
         m_indicator = new SolidIndicator(new RoboRioChannel(0), 40);
         m_controller = new XboxController(0);
-        new Trigger(m_controller::getAButton)
-                .whileTrue(Commands.run(() -> m_indicator.steady(Color.kGreen)))
-                .onFalse(Commands.run(() -> m_indicator.steady(Color.kBlack)));
-        new Trigger(m_controller::getBButton)
-                .whileTrue(Commands.run(() -> m_indicator.slow(Color.kOrangeRed)))
-                .onFalse(Commands.run(() -> m_indicator.steady(Color.kBlack)));
-        new Trigger(m_controller::getXButton)
-                .whileTrue(Commands.run(() -> m_indicator.fast(Color.kLightBlue)))
-                .onFalse(Commands.run(() -> m_indicator.steady(Color.kBlack)));
+        m_indicator.state(this::states);
+        m_indicator.event(m_controller::getXButton, Color.kAqua);
+        m_indicator.event(m_controller::getYButton, Color.kWhite);
+    }
+
+    /**
+     * Arbitrary logic to determine what color to show.
+     * 
+     * Some guidance for this logic: it would be good for states to be mutually
+     * exclusive, so that the order of evaluation here doesn't matter.
+     */
+    public Color states() {
+        if (m_controller.getAButton()) {
+            return Color.kRed;
+        } else if (m_controller.getBButton()) {
+            return Color.kGreen;
+        } else {
+            return Color.kBlack;
+        }
     }
 
     @Override
     public void robotPeriodic() {
-        Takt.update();
-        Cache.refresh();
         CommandScheduler.getInstance().run();
-        m_indicator.periodic();
     }
-
-    @Override
-    public void teleopInit() {
-    }
-
-    @Override
-    public void teleopPeriodic() {
-    }
-
-    @Override
-    public void teleopExit() {
-    }
-
 }
