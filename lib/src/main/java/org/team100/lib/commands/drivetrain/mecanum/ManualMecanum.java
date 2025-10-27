c78029bebdb09de6b81b7c4ef8b035b39bfe3d37
Joel Truher <joel@truher.org>
2025-10-26 19:45:47 -0700

    add mecanum input squashing

--- lib/src/main/java/org/team100/lib/commands/drivetrain/mecanum/ManualMecanum.java
+++ lib/src/main/java/org/team100/lib/commands/drivetrain/mecanum/ManualMecanum.java
@@ -11,17 +11,25 @@ import org.team100.lib.logging.LoggerFactory;
 import org.team100.lib.motion.drivetrain.kinodynamics.limiter.SwerveLimiter;
 import org.team100.lib.motion.mecanum.MecanumDrive100;
 import org.team100.lib.tuning.Mutable;
+import org.team100.lib.util.EnumChooser;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class ManualMecanum extends Command {
+    public enum InputShaping {
+        NONE,
+        CLIP,
+        SQUASH
+    }
+
     private final Supplier<Velocity> m_velocity;
     private final Mutable m_maxVX;
     private final Mutable m_maxVY;
     private final Mutable m_maxOmega;
     private final SwerveLimiter m_limiter;
     private final MecanumDrive100 m_drive;
+    private final EnumChooser<InputShaping> m_chooser;
 
     public ManualMecanum(
             LoggerFactory parent,
@@ -40,6 +48,7 @@ public class ManualMecanum extends Command {
         m_maxOmega = new Mutable(log, "maxOmega", maxOmega);
         m_limiter = limiter;
         m_drive = drive;
+        m_chooser = new EnumChooser<>("Input Shaping", InputShaping.NONE);
         addRequirements(drive);
     }
 
@@ -54,9 +63,13 @@ public class ManualMecanum extends Command {
         Velocity input = m_velocity.get();
         // clip the input to the diamond shape
         double y_x = m_maxVY.getAsDouble() / m_maxVX.getAsDouble();
-        Velocity clipped = input.diamond(1, y_x, poseRotation);
+        Velocity clippedOrSquashed = switch (m_chooser.get()) {
+            case NONE -> input;
+            case CLIP -> input.diamond(1, y_x, poseRotation);
+            case SQUASH -> input.squashedDiamond(1, y_x, poseRotation);
+        };
         GlobalVelocityR3 scaled = FieldRelativeDriver.scale(
-                clipped, m_maxVX.getAsDouble(), m_maxOmega.getAsDouble());
+                clippedOrSquashed, m_maxVX.getAsDouble(), m_maxOmega.getAsDouble());
         // Apply field-relative limits.
         if (Experiments.instance.enabled(Experiment.UseSetpointGenerator)) {
             scaled = m_limiter.apply(scaled);
