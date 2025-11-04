b1ed3178d906a8353116d88a4217f226e61ee43e
Joel Truher <joel@truher.org>
2025-11-04 10:59:27 -0800

    rotary example

---
+++ studies/rotary_example/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,31 @@
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+
+  public Robot() {
+
+  }
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+  @Override
+  public void teleopInit() {
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  @Override
+  public void teleopExit() {
+  }
+
+}
