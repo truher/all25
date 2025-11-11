a6e9e7529d5e6e9d497f495cbea0002e3106b5e0
marcelosin1010 <marcelo.sin1010@gmail.com>
2025-11-10 17:44:57 -0800

    played with turet

--- lib/src/main/java/org/team100/lib/targeting/Intercept.java
+++ lib/src/main/java/org/team100/lib/targeting/Intercept.java
@@ -1,8 +1,10 @@
 package org.team100.lib.targeting;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.team100.lib.geometry.GlobalVelocityR2;
+import org.team100.lib.util.Math100;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -29,8 +31,35 @@ public class Intercept {
             Translation2d targetPosition,
             GlobalVelocityR2 targetVelocity,
             double muzzleSpeed) {
+            
+                double T0x = targetPosition.getX() - robotPosition.getX();
+                double T0y = targetPosition.getY() - robotPosition.getY();
 
-        return Optional.empty();
+                double vTx = targetVelocity.x() - robotVelocity.x();
+                double vTy = targetVelocity.y() - robotVelocity.y();
+                double T0_dot_vT = T0x * vTx + T0y * vTy;
+                double vT_dot_vT = Math.pow(vTx, 2) + Math.pow(vTy, 2);
+
+                double C = Math.pow(T0x, 2) + Math.pow(T0y, 2);
+
+                
+                double B = 2.0 * T0_dot_vT;
+                double A = vT_dot_vT - Math.pow(muzzleSpeed, 2);
+                List<Double> solutions = Math100.solveQuadratic(A, B, C);
+                double theta = 0;
+                if(solutions.size()>0){
+                    for( int i=0; i<solutions.size(); i++){
+                        double t = solutions.get(i);
+                        double Ix = T0x + vTx * t;
+                        double Iy = T0y + vTy * t;
+                        theta = Math.atan2(Iy,Ix);
+                    }
+                   
+                }
+
+        
+
+        return Optional.of(new Rotation2d(theta));
     }
 
 }
