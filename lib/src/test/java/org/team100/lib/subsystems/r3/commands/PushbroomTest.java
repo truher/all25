990eb7c15c7ee5305512b675de02edd6c79c3805
Joel Truher <joel@truher.org>
2025-12-11 20:29:46 -0800

    fix the build

--- lib/src/test/java/org/team100/lib/subsystems/r3/commands/PushbroomTest.java
+++ lib/src/test/java/org/team100/lib/subsystems/r3/commands/PushbroomTest.java
@@ -9,7 +9,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
-public class PushbroonTest {
+public class PushbroomTest {
     private static final double DELTA = 0.001;
 
     @Test
@@ -56,22 +56,6 @@ public class PushbroonTest {
         Pose2d candidate = Pushbroom.candidate(new Pose2d(), target, robot, offset);
         assertEquals(1, candidate.getX(), DELTA);
         assertEquals(0, candidate.getY(), DELTA);
-        assertEquals(0, candidate.getRotation().getSin(), DELTA);
-        assertEquals(-1, candidate.getRotation().getCos(), DELTA);
-    }
-
-    @Test
-    void testCandidate4() {
-        Translation2d target = new Translation2d(1, 0);
-        // robot is facing -x
-        Pose2d robot = new Pose2d(0, 0.5, Rotation2d.k180deg);
-        // end effector is behind and to the left, facing backwards
-        Transform2d offset = new Transform2d(-0.5, 0.5, Rotation2d.k180deg);
-        // direction to the target is +x
-        // so goal should be (0.5, 0.5) facing -x
-        Pose2d candidate = Pushbroom.candidate(new Pose2d(), target, robot, offset);
-        assertEquals(0.5, candidate.getX(), DELTA);
-        assertEquals(0.5, candidate.getY(), DELTA);
         assertEquals(0, candidate.getRotation().getSin(), DELTA);
         assertEquals(-1, candidate.getRotation().getCos(), DELTA);
     }
